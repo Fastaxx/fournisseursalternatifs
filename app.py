c000531632a261d93be1a42e15ee903a63c55595
Fastaxx <Fastaxx@github.com>
2022-01-28 13:38:45 +0100

    l

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+
+header = st.container()
+
+with header:
+	st.title("Fournisseurs alternatifs")
+	st.markdown("""
+		Analyse 
+		""")
+	def load_data():
+		volume_echange_chemin ='/Users/Louis/GitHub/fournisseursalternatifs/volumes-echanges-re.csv'
+		data = pd.read_csv(volume_echange_chemin)
+		return load_data
+
+	df = load_data()
+	st.write(df)
